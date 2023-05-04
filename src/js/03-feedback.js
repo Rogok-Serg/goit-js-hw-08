ace990add6726ac07c430b06e2f3f3d985d5a479
Rogok-Serg <rogoksv@gmail.com>
2023-05-04 16:30:57 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,13 +3,13 @@ import throttle from 'lodash.throttle';
 const refs = {
   form: document.querySelector('.feedback-form'),
 }
-refs.form.addEventListener('input', onFormInputValue);
+refs.form.addEventListener('input', throttle(onFormInputValue, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 const LOCALSTORAGE_KEY = "feedback-form-state"
 
 populateInput()
 function onFormInputValue(evt) {
-  const email = evt.currentTarget.elements.email.value;
+    const email = evt.currentTarget.elements.email.value;
     const message = evt.currentTarget.elements.message.value;
     const formValue = {
     email,
@@ -28,7 +28,6 @@ function onFormSubmit(evt) {
 
 function populateInput() {
   const savedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  console.log(savedValue);
   if (savedValue) {
     refs.form.email.value = savedValue.email;
     refs.form.message.value = savedValue.message
